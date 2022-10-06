819edd9d55c42aeaaaa38e2933ec7838098a713f
eminkabacaoglu <eminkabacaoglu@gmail.com>
2022-10-06 19:09:55 +0300

    odev-1 update

---
+++ youTubeEgitim/src/youTubeEgitim/MilitaryCreditManager.java
@@ -0,0 +1,11 @@
+package youTubeEgitim;
+
+public class MilitaryCreditManager extends BaseCreditManager{
+
+	@Override
+	public void calculate() {
+		System.out.println("Military credit calculated");
+		
+	}
+
+}
---
+++ youTubeEgitim/src/youTubeEgitim/TeacherCreditManager.java
@@ -0,0 +1,11 @@
+package youTubeEgitim;
+
+public class TeacherCreditManager extends BaseCreditManager{
+
+	@Override
+	public void calculate() {
+		System.out.println("Teacher credit calculated");
+		
+	}
+
+}
